f660d502cc32b9a9167bf5469e7b9c9cddbe437f
shikeb1 <msmallick81@gmail.com>
2026-03-05 15:43:25 +0530

    final push with error handled

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from .serializers import StockPredictionSerializer
 from rest_framework import status
 from rest_framework.response import Response
@@ -16,14 +17,14 @@ from keras.models import load_model
 from sklearn.metrics import mean_squared_error, r2_score
 
 
-
 class StockPredictionAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
         serializer = StockPredictionSerializer(data=request.data)
         if serializer.is_valid():
             ticker = serializer.validated_data['ticker']
 
-            # Fetch the data from yfinance
             now = datetime.now()
             start = datetime(now.year-10, now.month, now.day)
             end = now
@@ -32,7 +33,7 @@ class StockPredictionAPIView(APIView):
                 return Response({"error": "No data found for the given ticker.",
                                  'status': status.HTTP_404_NOT_FOUND})
             df = df.reset_index()
-            # Generate Basic Plot
+
             plt.switch_backend('AGG')
             plt.figure(figsize=(12, 5))
             plt.plot(df.Close, label='Closing Price')
@@ -40,11 +41,8 @@ class StockPredictionAPIView(APIView):
             plt.xlabel('Days')
             plt.ylabel('Price')
             plt.legend()
-            # Save the plot to a file
-            plot_img_path = f'{ticker}_plot.png'
-            plot_img = save_plot(plot_img_path)
-            
-            # 100 Days moving average
+            plot_img = save_plot(f'{ticker}_plot.png')
+
             ma100 = df.Close.rolling(100).mean()
             plt.switch_backend('AGG')
             plt.figure(figsize=(12, 5))
@@ -54,10 +52,8 @@ class StockPredictionAPIView(APIView):
             plt.xlabel('Days')
             plt.ylabel('Price')
             plt.legend()
-            plot_img_path = f'{ticker}_100_dma.png'
-            plot_100_dma = save_plot(plot_img_path)
+            plot_100_dma = save_plot(f'{ticker}_100_dma.png')
 
-            # 200 Days moving average
             ma200 = df.Close.rolling(200).mean()
             plt.switch_backend('AGG')
             plt.figure(figsize=(12, 5))
@@ -68,39 +64,30 @@ class StockPredictionAPIView(APIView):
             plt.xlabel('Days')
             plt.ylabel('Price')
             plt.legend()
-            plot_img_path = f'{ticker}_200_dma.png'
-            plot_200_dma = save_plot(plot_img_path)
+            plot_200_dma = save_plot(f'{ticker}_200_dma.png')
 
-            # Splitting data into Training & Testing datasets
             data_training = pd.DataFrame(df.Close[0:int(len(df)*0.7)])
             data_testing = pd.DataFrame(df.Close[int(len(df)*0.7): int(len(df))])
 
-            # Scaling down the data between 0 and 1
             scaler = MinMaxScaler(feature_range=(0,1))
 
-            # Load ML Model
-            model = load_model('stock_prediction_model.keras')
+            model_path = os.path.join(settings.BASE_DIR.parent, 'resources', 'stock_prediction_model.keras')
+            model = load_model(model_path)
 
-            # Preparing Test Data
             past_100_days = data_training.tail(100)
             final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
             input_data = scaler.fit_transform(final_df)
 
-            x_test = []
-            y_test = []
+            x_test, y_test = [], []
             for i in range(100, input_data.shape[0]):
                 x_test.append(input_data[i-100: i])
                 y_test.append(input_data[i, 0])
             x_test, y_test = np.array(x_test), np.array(y_test)
 
-            # Making Predictions
             y_predicted = model.predict(x_test)
-
-            # Revert the scaled prices to original price
             y_predicted = scaler.inverse_transform(y_predicted.reshape(-1, 1)).flatten()
             y_test = scaler.inverse_transform(y_test.reshape(-1, 1)).flatten()
 
-            # Plot the final prediction
             plt.switch_backend('AGG')
             plt.figure(figsize=(12, 5))
             plt.plot(y_test, 'b', label='Original Price')
@@ -109,20 +96,11 @@ class StockPredictionAPIView(APIView):
             plt.xlabel('Days')
             plt.ylabel('Price')
             plt.legend()
-            plot_img_path = f'{ticker}_final_prediction.png'
-            plot_prediction = save_plot(plot_img_path)
+            plot_prediction = save_plot(f'{ticker}_final_prediction.png')
 
-            # Model Evaluation
-            # Mean Squared Error (MSE)
             mse = mean_squared_error(y_test, y_predicted)
-
-            # Root Mean Squared Error (RMSE)
             rmse = np.sqrt(mse)
-
-            # R-Squared
             r2 = r2_score(y_test, y_predicted)
-
-
             return Response({
                 'status': 'success',
                 'plot_img': plot_img,
@@ -132,4 +110,6 @@ class StockPredictionAPIView(APIView):
                 'mse': mse,
                 'rmse': rmse,
                 'r2': r2
-                })
+            })
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
